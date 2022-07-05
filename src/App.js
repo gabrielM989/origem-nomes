8c8ecf42ebfc6ffc7b112d15859d561e26c45121
gabrielM989 <gabriel.marques9899@gmail.com>
2022-07-05 18:55:07 -0300

    Desenvolvimento do projeto

--- src/App.js
+++ src/App.js
@@ -4,32 +4,36 @@ import "./style.css";
 import ListComponent from "./components/ListComponent";
 
 export default function App() {
-  const [nome, setNome] = React.useState([])
-  const [components, setComponets] = React.useState([])
+  const [nome, setNome] = React.useState("");
+  const [pais, setPais] = React.useState("");
 
   //console.log("nome: " + nome)
 
-  const clickNoBotao = () => {
-    console.log("click no botao nome")
-    //setNome(nome)
-    //<Paises name={nome}></Paises>
-    // setComponets([Paises])
-  }
-  console.log('components: ' + components)
+  function clickNoBotao(event, novoNome) {
+    //console.log("clickNoBotao event: " + event + " - novoNome: " + novoNome);
+    setPais(<Paises name={novoNome}></Paises>)    
+  };
+  //console.log("components: " + components);
  
   return (
     <div class="container">
       <h1>Pesquisa Origem Nomes!!!</h1>
-      <input 
-        type="text" placeholder="Informe o seu nome" name="nome"
-        value={nome} onChange = {(e) => setNome(e.target.value)} 
+      <input
+        type="text"
+        placeholder="Informe o seu nome"
+        name="nome"
+        value={nome}
+        onChange={e => setNome(e.target.value)}
       />
+
       <p></p>
-      <button class="btn" onClick={clickNoBotao}>Confirma</button>
-      <Paises name={nome}></Paises>
 
-      {/* {components.map((item, i) => ( <ListComponent text={item} /> ))} */}
+      <button 
+        onClick={event => clickNoBotao(event, nome)}>
+        Confirma
+      </button>
 
+      {pais}
     </div>
   )
 }
--- src/components/Paises.js
+++ src/components/Paises.js
@@ -1,34 +1,32 @@
-import * as React from 'react';
-import paisesJson from './listaDePaises.json';
+import * as React from "react";
+import paisesJson from "./listaDePaises.json";
 
 class Pais {
   constructor(name, code) {
-    this.name = name
-    this.code = code
+    this.name = name;
+    this.code = code;
   }
 }
 
 export default function Paises(props) {
-  const [paises, SetPaises] = React.useState([])
+  const [paises, setPaises] = React.useState([])
 
-  console.log("props.name: " + props.name)
+  console.log("Paises props.name: " + props.name);
   //console.log("paisesJson: " + paisesJson)
 
   let paisesObjetos = paisesJson.map((it) => {
-    //console.log("name: " + it.name + " - code: " + it.code + " - class: " + it.name.constructor.name) 
-    return new Pais(it.name, it.code)
-  })
+    //console.log("name: " + it.name + " - code: " + it.code + " - class: " + it.name.constructor.name)
+    return new Pais(it.name, it.code);
+  });
   //console.log("paisesObjetos: " + paisesObjetos)
 
-  const testNome = "gabriel"
   React.useEffect(() => {
-    //fetch('https://api.nationalize.io/?name=' + props.name)
-    fetch('https://api.nationalize.io/?name=' + testNome)
+    //console.log('useEffect props.name: ' + props.name)
+    fetch("https://api.nationalize.io/?name=" + props.name)
       .then((response) => response.json())
       .then((data) => {
         let countries = []
         let codigoPaises = []
-
         try {
           //console.log("data: " + data)
           //console.log("data.country: " + data.country)
@@ -37,40 +35,41 @@ export default function Paises(props) {
           codigoPaises = countries.map((c) => {
             //console.log("c: " + c.country_id)
             return c.country_id
-          })
-        } catch {
+          });
+          //console.log("codigoPaises: " + codigoPaises)
+        } catch (error) {
+          console.log('try catch error: ' + error)
         }
-
-        SetPaises(codigoPaises)
-      });
-  }, []);
+        setPaises(codigoPaises)
+      })  
+  }, [])
   //console.log('paises:' + paises)
 
-  let paisesDoNome = []
+  let paisesDoNome = [];
   paises.map((pais) => {
     //console.log("pais: " + pais)
     paisesObjetos.filter((it) => {
       //console.log("pais: " + pais + " code: " + it.code + " - name: " + it.name)
-      if (it.code === pais) { 
+      if (it.code === pais) {
         //console.log("2 pais: " + pais + " code: " + it.code + " - name: " + it.name)
-        paisesDoNome.push(it.name)
-        return true
+        paisesDoNome.push(it.name);
+        return true;
       }
-      return false
-    })
-  })
+      return false;
+    });
+  });
   //console.log('paisesDoNome:' + paisesDoNome)
 
   return (
     <div>
       <h2>Paises</h2>
       <div>
-        {paisesDoNome.map((pais) =>
+        {paisesDoNome.map((pais) => (
           <div>
             <span>{pais}</span>
           </div>
-        )}
+        ))}
       </div>
     </div>
-  )
+  );
 }
